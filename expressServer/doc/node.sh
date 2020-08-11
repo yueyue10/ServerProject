c29c336a9ccfc93676a1963cbbd6e9b00d924ae8
zhaoyj <1650432983@qq.com>
2020-08-11 16:33:21 +0800

    修改首页a标签

--- expressServer/doc/node.sh
+++ expressServer/doc/node.sh
@@ -5,8 +5,15 @@ time_str=$(date "+%Y-%m-%d %H:%M:%S")
 # 输出时间标识
 echo ---current time $time_str---
 # 命令路径
-Express_Path='/data/project/ServerProject/expressServer/bin/'
+Express_Path='/data/project/ServerProject/expressServer/'
+# 跳转到expressServer目录下，生成api文档
+echo -e "---cd ${Express_Path} && apidoc create doc--- \n"
 cd $Express_Path
+git pull
+apidoc -i routes/ -o public/apidoc/
+sleep 1
+# 跳转到expressServer/bin目录下，查找www文件
+cd ${Express_Path}bin/
 express_file=`ls`
 
 # 判断forever是否存在www
@@ -28,7 +35,7 @@ getForever(){
 
 # 如果文件夹中是否存在www，存在表示进入文件夹成功
 if [ $express_file = "www" ];then
-   echo -e "---cd $Express_Path--- \n"
+   echo -e "---cd ${Express_Path}bin/ && forever start server--- \n"
    rm -rf /root/.forever/forever.log
    getForever
    www_result=$?
