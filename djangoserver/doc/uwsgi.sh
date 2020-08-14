7eef2dedd972fd12f71169490592f9e6ce0b4d61
zhaoyj <1650432983@qq.com>
2020-08-14 19:35:49 +0800

    修改uwsgi.sh

--- djangoserver/doc/uwsgi.sh
+++ djangoserver/doc/uwsgi.sh
@@ -1,5 +1,6 @@
 echo 'start django server'
 django_path='/data/project/ServerProject/djangoserver'
 uwsgi_path=${django_path}/djangoserver/uwsgi.ini
-nohup uwsgi --ini ${uwsgi_path} > /root/.django/run.log 2&>1 &
 echo ${uwsgi_path}
+uwsgi --ini ${uwsgi_path}
+
