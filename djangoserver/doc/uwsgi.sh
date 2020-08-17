d3fb1730b93f48318004d19ae993c0f8bd945e4e
zhaoyj <1650432983@qq.com>
2020-08-17 14:43:06 +0800

    修改uwsgi配置

--- djangoserver/doc/uwsgi.sh
+++ djangoserver/doc/uwsgi.sh
@@ -1,39 +1,55 @@
 #! /bin/bash
-finishPort(){
-  array=()
-  for i in `netstat -anp|grep 8080`
-    do
-     array+=($i)
-    done
-  # 判断端口是否被占用
-  if [ ! -n "$array" ]; then
-    echo "---8080 is not use---"
-  else
-    echo "---port data_array $array ---"
-    prot_str=${array[6]}
-    port_index=`expr index ${prot_str} "/"`
-    echo "---fint port_pid_index $port_index ---"
-    port=${prot_str:0:port_index-1}
-    echo "---fint port_pid $port ---"
-    sudo kill -9 $port
-    echo "--- kill port_pid $port ---"
-  fi
-}
-echo '---start django server---'
+
 # 命令路径
 Django_path='/data/project/ServerProject/djangoserver'
-# 跳转到 djangoserver 目录下，更新生成api文档
-echo -e "---cd ${Django_path} && git update--- \n"
-cd $Django_path
-# --恢复本地修改，更新代码
-git checkout .
-git pull
-sleep 1
-# 生成 django 静态文件
-echo "---python3 manage.py collectstatic---"
-python3 manage.py collectstatic
-# uwsgi启动django服务
-uwsgi_path=${Django_path}/djangoserver/uwsgi.ini
-finishPort
-echo -e "\n\n ---uwsgi start django server---  \n\n"
-uwsgi --ini ${uwsgi_path}
+
+startUwsgi(){
+  # 跳转到 djangoserver 目录下，更新生成api文档
+  echo -e "---cd ${Django_path} && git update--- \n"
+  cd $Django_path
+  # --恢复本地修改，更新代码
+  git checkout .
+  git pull
+  sleep 1
+  # 生成 django 静态文件
+  echo "---python3 manage.py collectstatic---"
+  python3 manage.py collectstatic
+  # uwsgi启动django服务
+  echo -e "\n\n ---uwsgi start django server---  \n\n"
+  uwsgi --ini uwsgi/uwsgi.ini
+}
+
+stopUwsgi(){
+  cd $Django_path
+  uwsgi --stop uwsgi/uwsgi.pid
+}
+
+statusUwsgi(){
+  cd $Django_path
+  uwsgi --connect-and-read uwsgi/uwsgi.status
+}
+
+reloadUwsgi(){
+  cd $Django_path
+  uwsgi --reload uwsgi/uwsgi.pid
+}
+
+case "$1" in
+    'start')
+        startUwsgi
+        ;;
+    'stop')
+        stopUwsgi
+        ;;
+    'reload')
+        reloadUwsgi
+        ;;
+    'status')
+        statusUwsgi
+        ;;
+    *)
+
+    echo "Usage $0 { start | stop | status | reload }"
+    exit
+esac
+exit 0
